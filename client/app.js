9502160c013b1a7202c49ba12f828c098483d048
chenxiaobin <chenxiaobin@youzan.com>
2020-08-06 16:58:38 +0800

    feat: text;

--- client/app.js
+++ client/app.js
@@ -1,18 +1,14 @@
 import React from 'react';
-import UploadFiles from './pages/upload-files';
-import FetchFiles from './pages/fetch-files';
+// import UploadFiles from './pages/upload-files';
+import Fmr from './pages/fmr';
 import './app.scss';
 
 export default () => (
   <>
-    <h1 className="page-title">留图请留种</h1>
+    {/* <h1 className="page-title">留图请留种</h1>
     <div className="app-wrap">
       <UploadFiles />
-    </div>
-
-    {/* <h1 className="page-title">已上传文件</h1>
-    <div className="app-wrap">
-      <FetchFiles />
     </div> */}
+    <Fmr />
   </>
 );
---
+++ client/pages/fmr/index.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Button, Notify } from 'zent';
+
+function Fmr(props) {
+  const [loading, setLoading] = useState(false);
+  const handSubmitMR = () => {
+    const params = {
+      title: '测试',
+      id: 9890,
+      source_branch: 'feature/init',
+      target_branch: 'master',
+      assignee_id: 1518,
+    };
+    setLoading(true);
+    axios({
+      method: 'post',
+      data: params,
+      headers: {
+        'Private-Token': 'WkX_J3yGncfsNxUfHrKy',
+      },
+      url: 'https://gitlab.qima-inc.com/api/v4/projects/9890/merge_requests',
+    }).then(res => {
+      Notify.success('提交成功');
+      console.log('res', res);
+    }).catch(err => {
+      console.log('err', err);
+    }).finally(() => setLoading(false));
+  }
+  return (
+    <div>
+      <Button loading={loading} onClick={handSubmitMR}>提交MR</Button>
+    </div>
+  );
+}
+
+export default Fmr;
